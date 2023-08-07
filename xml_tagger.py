aac2f1ffdb507499cdd554b17b593d3ce5aaf0ad
Ernesto <ernesto@Earn-Work-MacBook-Pro.local>
2023-08-07 12:34:11 -0400

    removing layout writer due to new upload file spec

--- xml_tagger.py
+++ xml_tagger.py
@@ -6,59 +6,115 @@ import sys
 
 sys.dont_write_bytecode = True
 
-# Library of keyword Filenames we're looking for, as well as database of every county's FPN code
+# Counties paired with their respective information
 county_codes = {
-    "Charlotte.xml": "273", 
-    "Collier.xml": "272", 
-    "Hillsborough.xml": "275", 
-    "Lee.xml": "271",
-    "Manatee.xml": "270", 
-    "Orange.xml": "278", 
-    "Pasco.xml": "276", 
-    "Pinellas.xml": "274",
-    "Polk.xml": "277", 
-    "Sarasota.xml": "134", 
-    "Flagler.xml": "301", 
-    "Volusia.xml": "302",
-    "Sarasota Observer.xml": "408", 
-    "Longboat.xml": "409", 
-    "East County.xml": "410"
+    "Manatee.xml" :{
+        "ID" : "270",
+        "username": "BusObsMan",
+        "password" : "qgcU8a8D"
+    },
+
+    "Lee.xml" :{
+        "ID" : "271",
+        "username": "BusObsLee",
+        "password" : "4H2bMjVM"
+    },
+
+    "Collier.xml" :{
+        "ID" : "272",
+        "username": "BusObsCol",
+        "password" : "98m2HmbM"
+    },
+
+    "Charlotte.xml" :{
+        "ID" : "273",
+        "username": "BusObsCha",
+        "password" : "5KupMNm5"
+    },
+
+    "Pinellas.xml" :{
+        "ID" : "274",
+        "username": "BusObsPin",
+        "password" : "7wh4FSpA"
+    },
+
+    "Hillsborough.xml" :{
+        "ID" : "275",
+        "username": "BusObsHil",
+        "password" : "fRAZc293"
+    },
+
+    "Pasco.xml" :{
+        "ID" : "276",
+        "username": "BusObsPas",
+        "password" : "5NG6kCYy"
+    },
+
+    "Polk.xml" :{
+        "ID" : "277",
+        "username": "BusObsPol",
+        "password" : "8UkG6R3n"
+    },
+
+    "Orange.xml" :{
+        "ID" : "278",
+        "username": "BusObsOra",
+        "password" : "eMzH2V92"
+    },
+
+    "Volusia.xml" :{
+        "ID" : "329",
+        "username": "O1omAoDh",
+        "password" : "Wjjq4ygU"
+    },
+
+    "Flagler.xml" :{
+        "ID" : "330",
+        "username": "kVMopEwF",
+        "password" : "YGtp3LNH"
+    },
+
+    "Sarasota.xml" : {
+        "ID" : "134",
+        "username": "gulfcoast",
+        "password" : "busleagals"
+    },
 }
 
 # This function will handle writing of new layout XML file (only containing the layout object) in the new directory, on root of user-provided directory
 # Will require the PATH, NAME of matching XML file, and user-provided month, day and year
-def layout_file_writer(new_location, county_files, file_month, file_day, file_year):
+# def layout_file_writer(new_location, county_files, file_month, file_day, file_year):
     
-    # Open new file, which will only contain layout notice object
-    solo_xml_file = os.path.join(new_location, "fpn_upload_" + county_codes[county_files] + "." + file_year + file_month + file_day + ".xml")
+#     # Open new file, which will only contain layout notice object
+#     solo_xml_file = os.path.join(new_location, "fpn_upload_" + county_codes[county_files]["ID"] + "." + file_year + file_month + file_day + ".xml")
     
-    old_county = county_files.rstrip(".xml")
+#     old_county = county_files.rstrip(".xml")
 
-    # Create new file with county code that matches name via dictionary lookup
-    new_solo_layout_xml = open(solo_xml_file, "w")
+#     # Create new file with county code that matches name via dictionary lookup
+#     new_solo_layout_xml = open(solo_xml_file, "w")
     
-    # Write in the notice tag
-    new_solo_layout_xml.write("<xml>\n")
-    new_solo_layout_xml.write("  <notice>\n")
-    new_solo_layout_xml.write("    <subcategory_id>17</subcategory_id>\n")
-    new_solo_layout_xml.write("    <date>" + file_month + "/" + file_day + "/" + file_year + "</date>\n")
-    new_solo_layout_xml.write("    <text>Business Observer - " + old_county + " " + file_month + "/" + file_day + "/" + file_year + "</text>\n")
-    new_solo_layout_xml.write("    <image>" + file_year + "-" + file_month + "-" + file_day + "-" + old_county + ".pdf</image>\n")
-    new_solo_layout_xml.write('</notice>\n')
-    new_solo_layout_xml.write("</xml>\n")
+#     # Write in the notice tag
+#     new_solo_layout_xml.write("<xml>\n")
+#     new_solo_layout_xml.write("  <notice>\n")
+#     new_solo_layout_xml.write("    <subcategory_id>17</subcategory_id>\n")
+#     new_solo_layout_xml.write("    <date>" + file_month + "/" + file_day + "/" + file_year + "</date>\n")
+#     new_solo_layout_xml.write("    <text>Business Observer - " + old_county + " " + file_month + "/" + file_day + "/" + file_year + "</text>\n")
+#     new_solo_layout_xml.write("    <image>" + file_year + "-" + file_month + "-" + file_day + "-" + old_county + ".pdf</image>\n")
+#     new_solo_layout_xml.write('</notice>\n')
+#     new_solo_layout_xml.write("</xml>\n")
     
-    # Close the solo layout file
-    new_solo_layout_xml.close()
+#     # Close the solo layout file
+#     new_solo_layout_xml.close()
     
-    # Confirm to user 
-    print('New LAYOUT XML file successfully created.')
+#     # Confirm to user 
+#     print('New LAYOUT XML file successfully created.')
 
 
 # This function takes in old XML, and delivers a properly named copy, with the depricated username/password fields removed
 def file_writer(old_file, old_location, county_files, file_month, file_day, file_year):
     
 
-    new_file_ALPHA = os.path.join(old_location, "fpn_upload_" + county_codes[county_files] + "." + file_year + file_month + file_day + ".xml")
+    new_file_ALPHA = os.path.join(old_location, "fpn_upload_" + county_codes[county_files]["ID"] + "." + file_year + file_month + file_day + ".xml")
 
     # Create new file with county code that matches name via dictionary lookup
     new_file_object = open(new_file_ALPHA, "w")
@@ -98,14 +154,14 @@ def file_checker(user_directory):
     
     # This tries to find matches for XML files that exist in all dirs and subdirs of user provided directory
     for root, dirnames, files in os.walk(user_directory):
-        for county_files in files:
-            if county_files in county_codes:
+        for county_file in files:
+            if county_file in county_codes:
                 
                 # If any file is a match against the pre-determined naming conventions, trigger a new file in that location, 
-                old_XML = os.path.join(root, county_files)
+                old_XML = os.path.join(root, county_file)
 
                 # Subdirectory that the match was found in
-                old_location = old_XML.rstrip(county_files)
+                old_location = old_XML.rstrip(county_file)
                 
                 # This is the place we'd need to have a choice of whether to use separate or integrated XML PDF layout tags
                 
@@ -117,10 +173,10 @@ def file_checker(user_directory):
                     os.mkdir(tag_dest_folder)
 
                 # Function will create new XML file with ONLY the layout tag, and place it within the newly created directory
-                layout_file_writer(tag_dest_folder, county_files, file_month, file_day, file_year)
+                # layout_file_writer(tag_dest_folder, county_file, file_month, file_day, file_year)
 
                 # Function will create copy of old XML file, removing the deprecated username/password tags at top of file
-                file_writer(old_XML, old_location, county_files, file_month, file_day, file_year)
+                file_writer(old_XML, old_location, county_file, file_month, file_day, file_year)
 
 # If number of command line arguments is less than or greater than 2, program will quit. 
 # Only 1 user-provided CLI argument is necessary for program
